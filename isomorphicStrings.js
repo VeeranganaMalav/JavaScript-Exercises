4a66e9df4e039f9578bda88fc1e03763d76716c8
VeeranganaMalav <veeranganamalav01@gmail.com>
2022-06-17 19:13:21 +0530

    isomorphic strings

---
+++ isomorphicStrings.js
@@ -0,0 +1,28 @@
+/* Given two strings 's' and 't', determine if they are isomorphic.
+
+Two strings 's' and 't' are isomorphic if the characters in 's' can be replaced to get 't'.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself. */
+
+function isIsomorphic(s, t){
+    if(s.length !== t.length){
+        return false;
+    }
+
+    let sMap = new Map();
+    let tMap = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        if ((sMap.has(s[i]) && sMap.get(s[i]) !== t[i]) || (tMap.has(t[i]) && tMap.get(t[i]) !== s[i])){
+            return false;
+        }
+        else {
+            sMap.set(s[i], t[i]);
+            tMap.set(t[i], s[i]);
+        }
+    }
+    return true;
+}
+
+var result = isIsomorphic("foo", "ptt");
+console.log(result);
