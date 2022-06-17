0da2bdfc3e05de2ee0b8e03bda6aedf4e54d1341
VeeranganaMalav <veeranganamalav01@gmail.com>
2022-06-17 19:22:46 +0530

    combination of elements to sum upto target number

---
+++ combinationSum.js
@@ -0,0 +1,31 @@
+/* Given an array of distinct integers 'candidates' and a target integer 'target', return a list of all unique combinations of 'candidates' where the chosen numbers sum to 'target'. You may return the combinations in any order.
+
+The same number may be chosen from 'candidates' an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+It is guaranteed that the number of unique combinations that sum up to 'target' is less than 150 combinations for the given input. */
+
+function combinationSum(candidates, target){
+    let finalArray = [];
+
+    let backtrack = (remainingTarget = target, index = 0, arr = []) => {
+        if(remainingTarget === 0){
+            return finalArray.push([...arr]);
+        }
+
+        if(remainingTarget < 0){
+            return;
+        }
+
+        for(let i=index; i<candidates.length; i++){
+            arr.push(candidates[i]);
+            backtrack(remainingTarget-candidates[i], i, arr);
+            arr.pop();
+        }
+    }
+
+    backtrack();
+    return finalArray;
+}
+
+var result = combinationSum([2,3,6,7], 7);
+console.log(result);
